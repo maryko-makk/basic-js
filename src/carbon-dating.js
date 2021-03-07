cb370a742d382327608505f7e5f4ba28a08af7ac
maryko-makk <maryko-makk@yandex.ru>
2021-03-08 01:08:14 +0300

    add functions body

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -3,7 +3,12 @@ const CustomError = require("../extensions/custom-error");
 const MODERN_ACTIVITY= 15; 
 const HALF_LIFE_PERIOD= 5730;
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function dateSample(sampleActivity) {
+  let k = HALF_LIFE_PERIOD / 0.693;
+  let n = parseFloat(sampleActivity);
+  if (typeof sampleActivity == "string" && n > 0 && n < MODERN_ACTIVITY) {
+  return Math.ceil(Math.log(MODERN_ACTIVITY / n) * k);
+  } else {
+    return false;
+  }
 };
--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,43 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function transform(arr) {
+  //--discard-next исключает следующий за ней элемент исходного массива из преобразованного массива.
+  //--discard-prev исключает предшествующий ей элемент исходного массива из преобразованного массива.
+  //--double-next удваивает следующий за ней элемент исходного массива в преобразованном массиве.
+  //--double-prev удваивает предшествующий ей элемент исходного массива в преобразованном массиве.
+
+  const d1 = "--discard-next";
+  const d2 = "--discard-prev";
+  const d3 = "--double-next";
+  const d4 = "--double-prev";
+
+  if (!arr) {
+    throw new Error('Error');
+  }
+  let new_arr = [];
+  for (let i=0; i<arr.length; i++) {
+    let element = arr[i];
+    switch(element) {
+      case d1: i++;
+      break;
+      case d2: 
+      if (new_arr.length != 0 && arr[i-2] != d1) {
+        new_arr.pop();
+      };
+      break;
+      case d3: 
+      if (i+1<arr.length) {
+        new_arr.push(arr[i+1]);
+      };
+      break;
+      case d4: 
+      if (i-1 > -1 && arr[i-2] != d1) {
+        new_arr.push(arr[i-1]);
+      };
+      break;
+      default: new_arr.push(element);
+      break;
+    }
+  }
+  return new_arr;
 };
